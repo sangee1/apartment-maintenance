9b7db4ab4c67af34273a9d6900f76f49f38116f0
sangee1 <sangeetha.krishnaprasad@gmail.com>
2026-04-23 16:35:57 +0530

    Create HomeController.java

---
+++ backend/AptMaintenance/src/main/java/com/shangrila/aptmaintenance/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.shangrila.aptmaintenance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Apartment Maintenance Backend Running 🚀";
+    }
+}
