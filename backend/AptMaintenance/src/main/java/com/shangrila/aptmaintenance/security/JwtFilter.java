275c0ced61b69922ec10f1208f32a7e7b051c008
sangee1 <sangeetha.krishnaprasad@gmail.com>
2026-04-24 12:24:52 +0530

    Update JwtFilter.java

--- backend/AptMaintenance/src/main/java/com/shangrila/aptmaintenance/security/JwtFilter.java
+++ backend/AptMaintenance/src/main/java/com/shangrila/aptmaintenance/security/JwtFilter.java
@@ -31,6 +31,13 @@ public class JwtFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
+        String path = request.getRequestURI();
+
+        // ✅ Allow auth endpoints WITHOUT token
+        if (path.startsWith("/api/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         String authHeader = request.getHeader("Authorization");
 
